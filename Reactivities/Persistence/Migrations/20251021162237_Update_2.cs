6ca94ac3cd7a7805326f7b99a30362b0f068e713
Andor Mihaly <andor_mihaly@yahoo.com>
2025-10-21 20:40:47 +0300

    Sec (2) - Add Context and Controller

---
+++ Reactivities/Persistence/Migrations/20251021162237_Update_2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Activities",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Activities");
+        }
+    }
+}
