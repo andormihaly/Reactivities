e75e1cfd189d6e811527db0b5fabc9259ec6c35c
Andor Mihaly <andor_mihaly@yahoo.com>
2025-10-28 20:54:52 +0200

    Sec(5)  - Add some CRUD operation

--- Reactivities/client/vite.config.ts
+++ Reactivities/client/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import mkcert from 'vite-plugin-mkcert'
+
+//import mkcert from 'vite-plugin-mkcert' ->because not works with node 22
+
 // https://vite.dev/config/
 export default defineConfig({
   server :{
     port:3000
   },
-  plugins: [react(),mkcert()],
+  plugins: [react(),/*mkcert()*/],
 })
