9ebbf4868325a4f08176dad673e7985f3410f410
Andor Mihaly <andor_mihaly@yahoo.com>
2025-11-04 16:08:46 +0200

    Sec(8) Add styling

--- Reactivities/client/src/app/router/Routes.tsx
+++ Reactivities/client/src/app/router/Routes.tsx
@@ -3,7 +3,7 @@ import App from "../layout/App"
 import HomePage from "../../features/home/HomePage";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityForm from "../../features/activities/dashboard/form/ActivityForm";
-import ActivityDetails from "../../features/activities/dashboard/details/ActivityDetails";
+import ActivityDetailPage from "../../features/activities/dashboard/details/ActivityDetailPage";
 
 export const router = createBrowserRouter([
     {
@@ -13,7 +13,7 @@ export const router = createBrowserRouter([
             {path:'', element:<HomePage/>},
             {path:'activities', element:<ActivityDashboard></ActivityDashboard>},
             {path:'createActivity', element:<ActivityForm  key='create'></ActivityForm>},
-            {path:'activities/:id', element:<ActivityDetails></ActivityDetails>},
+            {path:'activities/:id', element:<ActivityDetailPage></ActivityDetailPage>},
             {path:'manage/:id', element:<ActivityForm></ActivityForm>},
         ]
     }
---
+++ Reactivities/client/src/lib/util/util.ts
@@ -0,0 +1,7 @@
+import { format, type DateArg } from "date-fns";
+
+export function formatDate(date:DateArg<Date>) {
+
+    return format(date, 'dd MMM yyyy h:mm a')
+}
+    
